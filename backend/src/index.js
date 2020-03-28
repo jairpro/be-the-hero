485a45153e58605a1055186ff3e8dd636c59743d
jairpro <jairedias@yahoo.com.br>
2020-03-28 16:14:12 -0300

    Back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cor = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+// app.use(cors({
+//   origin: 'http://bethehero.jairpro.tk'
+// }));
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3340);
